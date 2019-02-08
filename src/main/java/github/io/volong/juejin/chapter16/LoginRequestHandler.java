13d1f8db5033546fd81424d258213d8d98c6fa96
volong <yhlona523@gmail.com>
2019-02-08 22:55:31 +0800

    chapter16

---
+++ src/main/java/github/io/volong/juejin/chapter16/LoginRequestHandler.java
@@ -0,0 +1,13 @@
+package github.io.volong.juejin.chapter16;
+
+import github.io.volong.juejin.chapter08.LoginRequestPacket;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class LoginRequestHandler extends SimpleChannelInboundHandler<LoginRequestPacket> {
+
+     
+
+
+
+
+}
