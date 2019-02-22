297b0083f5302fcb0ae7a6dad521988bba1cad77
volong <yhlona523@gmail.com>
2019-02-22 22:08:14 +0800

    netty in gitchat

---
+++ src/main/java/github/io/volong/gitchat/chapter04/NettyClientHandler.java
@@ -0,0 +1,66 @@
+package github.io.volong.gitchat.chapter04;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final byte[] request;
+
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public NettyClientHandler() {
+        request = "hello server,im a clinet".getBytes();
+    }
+
+    /**
+     *
+     * 当客户端与服务端连接建立完毕后被回调
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+        System.out.println("--- client already connected ---");
+
+        ByteBuf message = null;
+
+        for (int i = 0; i < 1; i++) {
+            message = Unpooled.buffer(request.length);
+            message.writeBytes(request);
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    /**
+     *
+     * 客户端接受 buffer 里面的数据就绪后被回调
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf message = (ByteBuf) msg;
+
+        byte[] response = new byte[message.readableBytes()];
+        message.readBytes(response);
+
+        System.out.println(atomicInteger.getAndIncrement() + " receive from server:" + new String(response));
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/github/io/volong/gitchat/chapter04/NettyServer.java
@@ -0,0 +1,52 @@
+package github.io.volong.gitchat.chapter04;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class NettyServer {
+
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 创建主从 reactor 线程池
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boosGroup, workerGroup) // 设置主从线程池
+             .channel(NioServerSocketChannel.class) // 指定用于创建服务端 NIO 通道的 Class 对象
+             .option(ChannelOption.SO_BACKLOG, 100)
+             .handler(new LoggingHandler(LogLevel.INFO)) // 设置日志
+             .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                 @Override
+                 protected void initChannel(SocketChannel ch) throws Exception {
+                     ChannelPipeline p = ch.pipeline();
+
+                     p.addLast(new NettyServerHandler());
+                 }
+             });
+
+            // 启动服务器
+            ChannelFuture f = b.bind(PORT).sync();
+            System.out.println("--- server started ---");
+
+            // 同步等待服务 socket 关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅关闭线程池组
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
